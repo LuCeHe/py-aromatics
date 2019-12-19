d4151f80f2024d2189f0d74d770bf264c368733f
LuCeHe <manucelotti@gmail.com>
2019-12-18 21:01:57 -0500

    restructure folders and files

---
+++ KerasTools/convenience_layers.py
@@ -0,0 +1,69 @@
+import tensorflow as tf
+import tensorflow.keras.backend as K
+
+from tensorflow.keras.layers import Input, Embedding, \
+    LSTM, Lambda, Dense, Layer
+from tensorflow.keras.models import Model
+
+from GenericTools.TFTools.convenience_operations import slice_from_to, clip_layer, replace_column
+
+
+class ExpandDims(object):
+
+    def __init__(self, axis):
+        self.axis = axis
+
+    def __call__(self, inputs):
+        def ed(tensor, axis):
+            expanded = K.expand_dims(tensor, axis=axis)
+            return expanded
+
+        return Lambda(ed, arguments={'axis': self.axis})(inputs)
+
+
+class Slice(object):
+
+    # axis parameter is not functional
+    def __init__(self, axis, initial, final):
+        self.axis, self.initial, self.final = axis, initial, final
+
+    def __call__(self, inputs):
+        return Lambda(slice_from_to, arguments={'initial': self.initial, 'final': self.final})(inputs)
+
+
+class Clip(object):
+
+    def __init__(self, min_value=0., max_value=1.):
+        self.min_value, self.max_value = min_value, max_value
+
+    def __call_(self, inputs):
+        return Lambda(clip_layer, arguments={'min_value': self.min_value, 'max_value': self.max_value})(inputs)
+
+
+class ReplaceColumn(Layer):
+
+    def __init__(self, column_position, **kwargs):
+        super(ReplaceColumn, self).__init__(**kwargs)
+
+        self.column_position = column_position
+
+    def call(self, inputs, training=None):
+        matrix, column = inputs
+
+        matrix = tf.cast(matrix, dtype=tf.float32)
+        column = tf.cast(column, dtype=tf.float32)
+        new_matrix = replace_column(matrix, column, self.column_position)
+        new_matrix = tf.cast(new_matrix, dtype=tf.int32)
+        return new_matrix
+
+
+def predefined_model(vocab_size, emb_dim, units=256):
+    embedding = Embedding(vocab_size, emb_dim, mask_zero='True')
+    lstm = LSTM(units, return_sequences=False)
+
+    input_question = Input(shape=(None,), name='discrete_sequence')
+    embed = embedding(input_question)
+    lstm_output = lstm(embed)
+    softmax = Dense(vocab_size, activation='softmax')(lstm_output)
+
+    return Model(inputs=input_question, outputs=softmax)
---
+++ TFTools/convenience_operations.py
@@ -0,0 +1,57 @@
+import tensorflow as tf
+
+
+def dynamic_zeros(x, d):
+    batch_size = tf.shape(x)[0]
+    return tf.zeros(tf.stack([batch_size, 1, d]))
+
+
+def dynamic_ones(x, d):
+    batch_size = tf.shape(x)[0]
+    return tf.ones(tf.stack([batch_size, 1, d]))
+
+
+def dynamic_fill(x, d, value):
+    batch_size = tf.shape(x)[0]
+    return tf.fill(tf.stack([batch_size, 1, d]), value)
+
+
+def dynamic_filler(batch_as, d, value):
+    batch_size = tf.shape(batch_as)[0]
+    return tf.fill(tf.stack([batch_size, d]), value)
+
+
+def dynamic_one_hot(x, d, pos):
+    batch_size = tf.shape(x)[0]
+    one_hots = tf.ones(tf.stack([batch_size, 1, d])) * tf.one_hot(pos, d)
+    return one_hots
+
+
+def slice_(x):
+    return x[:, :-1, :]
+
+
+def slice_from_to(x, initial, final):
+    # None can be used where initial or final, so
+    # [1:] = [1:None]
+    return x[:, initial:final]
+
+
+def clip_layer(inputs, min_value, max_value):
+    eps = .5e-6
+    clipped_point = tf.clip_by_value(inputs, min_value + eps, max_value - eps)
+    return clipped_point
+
+
+def replace_column(matrix, new_column, r):
+    dynamic_index = tf.cast(tf.squeeze(r), dtype=tf.int64)
+    matrix = tf.cast(matrix, dtype=tf.float32)
+    new_column = tf.cast(new_column, dtype=tf.float32)
+    num_cols = tf.shape(matrix)[1]
+    # new_matrix = tf.assign(matrix[:, dynamic_index], new_column)
+    index_row = tf.stack([tf.eye(num_cols, dtype=tf.float32)[dynamic_index, :]])
+    old_column = matrix[:, dynamic_index]
+    new = tf.matmul(tf.stack([new_column], axis=1), index_row)
+    old = tf.matmul(tf.stack([old_column], axis=1), index_row)
+    new_matrix = (matrix - old) + new
+    return new_matrix
