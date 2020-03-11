39a8a1c930fa447ca3f348b2124d996125cbc15f
LuCeHe <manucelotti@gmail.com>
2020-03-11 13:16:35 +0100

    ready for long loop on Puget

--- KerasTools/convenience_tools.py
+++ KerasTools/convenience_tools.py
@@ -3,14 +3,18 @@ import matplotlib.pyplot as plt
 
 def plot_history(history, plot_filename, epochs):
     if epochs > 0:
-        # plot training and validation losses
-        fig = plt.figure(figsize=(8, 8))
-        ax = fig.add_subplot(111)
-        ax.plot(history.history['loss'], label='train')
-        ax.plot(history.history['val_loss'], label='val')
-        ax.set_title('model loss')
-        ax.set_ylabel('loss')
-        ax.set_xlabel('epoch')
-        ax.legend()
+        keys = history.history.keys()
+        keys = [k for k in keys if not 'val' in k]
+        fig, axs = plt.subplots(len(keys), figsize=(8, 8))
+
+        fig.suptitle(plot_filename)
+        for k, ax in zip(keys, axs):
+            # plot training and validation losses
+            ax.plot(history.history[k], label='train ' + k)
+            ax.plot(history.history['val_' + k], label='val ' + k)
+            ax.set_ylabel(k)
+            ax.set_xlabel('epoch')
+            ax.legend()
+
         fig.savefig(plot_filename, bbox_inches='tight')
 
