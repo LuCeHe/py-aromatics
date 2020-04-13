f83a91d77e716c9e93afb3e4fc24e5c023faeb8f
LuCeHe <manucelotti@gmail.com>
2020-04-13 17:27:01 +0200

    minor changes

--- KerasTools/convenience_tools.py
+++ KerasTools/convenience_tools.py
@@ -1,20 +1,26 @@
 import os
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+
 
 def plot_history(history, plot_filename, epochs):
     if epochs > 0:
         keys = history.history.keys()
         keys = [k for k in keys if not 'val' in k]
-        fig, axs = plt.subplots(len(keys), figsize=(8, 8))
+        fig, axs = plt.subplots(len(keys), figsize=(8, 8), sharex='all',
+                            gridspec_kw={'hspace': 0})
 
         fig.suptitle(plot_filename)
         for k, ax in zip(keys, axs):
             # plot training and validation losses
-            ax.plot(history.history[k], label='train ' + k)
-            ax.plot(history.history['val_' + k], label='val ' + k)
+            try:
+                ax.plot(history.history['val ' + k], label='val ' + k)
+                ax.plot(history.history[k], label='train ' + k)
+            except:
+                ax.plot(history.history[k], label=k)
             ax.set_ylabel(k)
-            ax.set_xlabel('epoch')
             ax.legend()
-
+        ax.set_xlabel('epoch')
+        ax.xaxis.set_major_locator(MaxNLocator(integer=True))
         fig.savefig(plot_filename, bbox_inches='tight')
 
