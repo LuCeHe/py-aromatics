2024c9616e8f592bed4007048029f0dec6241b02
LuCeHe <manucelotti@gmail.com>
2020-09-10 12:07:32 +0200

    small changes

--- KerasTools/gradient_tensorboard.py
+++ KerasTools/gradient_tensorboard.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 
 
 class ExtendedTensorBoard(tf.keras.callbacks.TensorBoard):
@@ -6,7 +7,23 @@ class ExtendedTensorBoard(tf.keras.callbacks.TensorBoard):
         super().__init__(*args, **kwargs)
 
         # here we use test data to calculate the gradients
-        self._x_batch = tf.convert_to_tensor(val_data[0], dtype=tf.float32)
+        self.multiple_inputs = False
+        if isinstance(val_data[0], list):
+            self.multiple_inputs = True
+            self.splits = len(val_data[0])
+            self._x_batch = []
+            for v in val_data[0]:
+                self._x_batch.append(tf.convert_to_tensor(v, dtype=tf.float32))
+            # self._x_batch = tf.convert_to_tensor(self._x_batch)
+            self.shapes = [v.shape for v in val_data[0]]
+            print(self.shapes)
+            flattened_vs = [v.flatten() for v in val_data[0]]
+            self.flattened_shapes = [len(v) for v in flattened_vs]
+            print(self.flattened_shapes)
+            v = np.concatenate(flattened_vs)
+            self._x_batch = tf.convert_to_tensor(v)
+        else:
+            self._x_batch = tf.convert_to_tensor(val_data[0], dtype=tf.float32)
         self._y_batch = tf.convert_to_tensor(val_data[1], dtype=tf.float32)
 
     def _log_gradients(self, epoch):
@@ -15,14 +32,22 @@ class ExtendedTensorBoard(tf.keras.callbacks.TensorBoard):
 
         with writer.as_default(), tf.GradientTape() as g:
             g.watch(self._x_batch)
-            _y_pred = self.model(self._x_batch)  # forward-propagation
+            if self.multiple_inputs:
+                fx = tf.split(self._x_batch, self.flattened_shapes)
+                x = [tf.reshape(f, s) for f, s in zip(fx, self.shapes)]
+                print(x)
+            else:
+                x = self._x_batch
+
+            _y_pred = self.model(x)  # forward-propagation
             loss = self.model.loss(y_true=self._y_batch, y_pred=_y_pred)  # calculate loss
             gradients = g.gradient(loss, self.model.trainable_weights)  # back-propagation
 
             # In eager mode, grads does not have name, so we get names from model.trainable_weights
             for weights, grads in zip(self.model.trainable_weights, gradients):
                 tf.summary.histogram(
-                    weights.name.replace(':', '_') + '_grads', data=grads, step=epoch)
+                    weights.name.replace(':', '_') + '_grads',
+                    data=grads, step=epoch)
 
         writer.flush()
 
