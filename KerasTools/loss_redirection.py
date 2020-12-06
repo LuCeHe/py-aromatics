1294b829ac3ba0678d8adf7460aedaed93478464
lucehe <luca.herrtti@gmail.com>
2020-12-06 20:57:37 +0100

    mc

--- KerasTools/loss_redirection.py
+++ KerasTools/loss_redirection.py
@@ -1,9 +1,17 @@
 from GenericTools.KerasTools.focal_loss import categorical_focal_loss
 import tensorflow_addons as tfa
+import tensorflow as tf
+
 
 def get_loss(loss_name):
     if 'categorical_crossentropy' in loss_name:
         loss = 'categorical_crossentropy'
+        loss = tf.keras.losses.CategoricalCrossentropy(
+            from_logits=False,
+            label_smoothing=0.1,
+            reduction="auto",
+            name="categorical_crossentropy",
+        )
     elif 'categorical_focal_loss:' in loss_name:
         n_out = int(loss_name.split(':')[1])
         loss = categorical_focal_loss(alpha=[[1 / n_out] * n_out], gamma=2)
