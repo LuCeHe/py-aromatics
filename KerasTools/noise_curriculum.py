acb3ae44ae4a7946e50d93d9c2d49b4d06ce1e54
LuCeHe <manucelotti@gmail.com>
2021-01-29 15:55:27 +0100

    orthogonal for transformers, scheduled noise on transformer and scheduled noise working confirmed

--- KerasTools/noise_curriculum.py
+++ KerasTools/noise_curriculum.py
@@ -13,10 +13,8 @@ class InputSensitiveGaussianNoise(tf.keras.layers.GaussianNoise):
     def call(self, inputs, training=None):
         if not training is None:
             tf.keras.backend.set_learning_phase(training)
-        # print('what?!', tf.keras.backend.learning_phase())
         is_train = tf.cast(tf.keras.backend.learning_phase(), tf.float32)
 
-        # print('training: ', training, is_train)
         std = tf.math.reduce_std(inputs)
         output = inputs + is_train * self.stddev * std * tf.random.normal(tf.shape(inputs))
 
@@ -32,15 +30,9 @@ class NoiseSchedule(tf.keras.callbacks.Callback):
         self.stddev = stddev
 
     def on_epoch_begin(self, epoch, logs=None):
-        # print('epoch: ', epoch)
-        std = tf.keras.backend.get_value(self.stddev)
-        # print(std)
         if epoch < self.epochs / 2 - 1:
             tf.keras.backend.set_value(self.stddev, 0.)
         else:
             # 0 to .6
             portion = (2 * (epoch + 1) / self.epochs - 1)
             tf.keras.backend.set_value(self.stddev, .6 * portion)
-            # print(portion, self.stddev)
-        std = tf.keras.backend.get_value(self.stddev)
-        # print(std)
