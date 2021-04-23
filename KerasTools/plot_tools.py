3bc4bd178df4126d3bb519112c5ef1a641161329
LuCeHe <manucelotti@gmail.com>
2021-04-23 19:21:00 +0200

    minor changes

--- KerasTools/plot_tools.py
+++ KerasTools/plot_tools.py
@@ -4,20 +4,8 @@ from matplotlib.ticker import MaxNLocator
 import numpy as np
 
 
-def standardize_dataset_names(initial_name):
-    name = ''
-    if initial_name == 'ptb':
-        name = 'PTB'
-    elif initial_name == 'heidelberg':
-        name = 'SHD'
-    elif initial_name == '_s_mnist':
-        name = 'sMNIST'
-    elif initial_name == '_ps_mnist':
-        name = 'psMNIST'
-    return name
-
 def plot_history(histories, plot_filename, epochs, method_names=None, save=True, show=False, bkg_color='white',
-                 keys_to_skip=[], column_id=[]):
+                 metrics_to_show=[], column_id=[], colors=None):
     if not isinstance(histories, list): histories = [histories]
 
     if not method_names is None:
@@ -31,25 +19,25 @@ def plot_history(histories, plot_filename, epochs, method_names=None, save=True,
         keys = list(set(keys))
         keys = [k for k in keys if not 'val' in k]
 
-        if keys_to_skip:
-            keys = [k for k in keys if not k in keys_to_skip]
+        if metrics_to_show:
+            keys = [k for k in keys if k in metrics_to_show]
 
-        print(keys)
         n_columns = len(column_id) if not len(column_id) == 0 else 1
-        fig, axs = plt.subplots(len(keys), n_columns, figsize=(8, 8), gridspec_kw={'hspace': 0})
+        fig, axs = plt.subplots(len(keys), n_columns, figsize=(20, 9), gridspec_kw={'hspace': 0})
         axs = axs if type(axs) is np.ndarray else [axs]
 
         # fig.suptitle(plot_filename)
 
         # colors = plt.cm.gist_ncar(np.linspace(0, 1, len(histories)))
-        cm = plt.get_cmap('Reds')
-        colors = cm(np.linspace(1, 0, len(histories)))
-        np.random.shuffle(colors)
+        if colors is None:
+            cm = plt.get_cmap('Reds')
+            colors = cm(np.linspace(1, 0, len(histories)))
+            np.random.shuffle(colors)
 
         lines = []
 
         if method_names is None:
-            method_names = [None]*len(histories)
+            method_names = [None] * len(histories)
         for history, c, m in zip(histories, colors, method_names):
             print(m)
             for i, k in enumerate(keys):
@@ -58,8 +46,6 @@ def plot_history(histories, plot_filename, epochs, method_names=None, save=True,
 
                 ax = axs[(i, column) if column_id else i]
                 ax.set_facecolor(bkg_color)
-                if i == 0 and column_id:
-                    ax.set_title(standardize_dataset_names(column_id[column]))
 
                 # plot training and validation losses
                 if k in history.history.keys():
@@ -88,3 +74,5 @@ def plot_history(histories, plot_filename, epochs, method_names=None, save=True,
 
         if show:
             plt.show()
+
+        return fig, axs
