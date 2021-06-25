b597259a2ee0500b2c4c6f5392d8b418d57c88eb
LuCeHe <manucelotti@gmail.com>
2021-06-25 22:19:46 +0200

    minor changes

--- KerasTools/esoteric_tasks/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-
-FILENAME = os.path.realpath(__file__)
-CDIR = os.path.dirname(FILENAME)
-DATADIR = os.path.abspath(os.path.join(CDIR, '..', 'data',))
-
-if not os.path.isdir(DATADIR):
-    os.mkdir(DATADIR)
