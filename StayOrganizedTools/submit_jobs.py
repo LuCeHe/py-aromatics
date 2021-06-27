a9acfac74348cfc302d0d01714cc1b8f49de9767
LuCeHe <manucelotti@gmail.com>
2021-06-27 12:31:10 +0200

    minor changes

---
+++ StayOrganizedTools/submit_jobs.py
@@ -0,0 +1,26 @@
+import os, itertools
+
+
+def run_experiments(experiments, init_command='python language_main.py with ',
+                    run_string='sbatch run_tf2.sh ', is_argparse=False):
+    ds = dict2iter(experiments)
+    print('Number jobs: {}'.format(len(ds)))
+    for d in ds:
+        a = '--' if is_argparse else ''
+        config_update = ''.join([a + '{}={} '.format(k, v) for k, v in d.items()])
+        command = init_command + config_update
+
+        command = run_string + "'{}'".format(command)
+        command = command.replace('  ', ' ')
+        print(command)
+        os.system(command)
+    print('Number jobs: {}'.format(len(ds)))
+
+
+def dict2iter(experiments):
+    full_ds = []
+    for experiment in experiments:
+        c = list(itertools.product(*experiment.values()))
+        ds = [{k: v for k, v in zip(experiment.keys(), i)} for i in c]
+        full_ds.extend(ds)
+    return full_ds
