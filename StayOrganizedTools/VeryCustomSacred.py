819ed6ed593e0115798194d3bbbd501b55eb99ef
LuCeHe <manucelotti@gmail.com>
2021-09-24 08:12:07 -0400

    minor changes

--- StayOrganizedTools/VeryCustomSacred.py
+++ StayOrganizedTools/VeryCustomSacred.py
@@ -1,4 +1,6 @@
 import logging, os, pathlib, shutil, time
+import datetime
+from tqdm import tqdm
 from time import strftime, localtime
 import tensorflow as tf
 
@@ -18,7 +20,7 @@ class CustomFileStorageObserver(FileStorageObserver):
             timestamp = "{}-{}_".format(time_string, ex_info['name'])
             options = '_'.join(meta_info['options']['UPDATE'])
             self.updated_config = meta_info['options']['UPDATE']
-            #run_id = timestamp + options
+            # run_id = timestamp + options
             run_id = timestamp
 
             # update the basedir of the observer
@@ -46,13 +48,14 @@ class CustomFileStorageObserver(FileStorageObserver):
 def CustomExperiment(experiment_name, base_dir=None, GPU=None, seed=10, ingredients=[]):
     import numpy as np
     random_string = ''.join([str(r) for r in np.random.choice(10, 4)]) + '-'
-    ex = Experiment(name=random_string + experiment_name, base_dir=base_dir, ingredients=ingredients, save_git_info=False)
+    ex = Experiment(name=random_string + experiment_name, base_dir=base_dir, ingredients=ingredients,
+                    save_git_info=False)
     ex.observers.append(CustomFileStorageObserver("experiments"))
 
     ex.captured_out_filter = apply_backspaces_and_linefeeds
 
     # create convenient folders for all experiments
-    basic_folders =  ['data', 'experiments', 'good_experiments']
+    basic_folders = ['data', 'experiments', 'good_experiments']
     for path in basic_folders:
         complete_path = os.path.join(base_dir, path)
         if not os.path.isdir(complete_path):
@@ -99,15 +102,73 @@ def remove_folder(folder_path):
     shutil.rmtree(folder_path, ignore_errors=True)
 
 
-if __name__ == '__main__':
-    a = [26.2, 37.0, 42.9, 47.5,
-         55.0, 57.9, 58.4, 66.7]
+def summarize(track_params=[]):
+    CDIR = os.path.dirname(os.path.realpath(__file__))
+    CDIR = os.path.abspath(os.path.join(*[CDIR, '..', ]))
+    EXPERIMENTS = os.path.join(*[CDIR, 'experiments'])
 
-    for f in a:
-        new_f = f / 120 / 1e-9 * 1e6 * 5
-        c = 3e8
-        new_lambd = c / new_f
-        new_f = int(10 * new_f * 1e-15) / 10
-        new_lambd = int(10 * new_lambd * 1e9) / 10
-        # print('{} MHz -> {} Hz'.format(f, new_f))
-        print('{} MHz -> {} pHz = {} nm'.format(f, new_f, new_lambd))
+    time_string = timeStructured()
+
+    summary_file = os.path.join(*[EXPERIMENTS, time_string + '_summary_slurms.txt'])
+
+    ds = [d for d in os.listdir(CDIR) if 'slurm' in d]
+    ds = sorted(ds)
+    print(ds)
+
+    for d in tqdm(ds):
+        update = False
+        d_path = os.path.join(*[CDIR, d])
+        with open(d_path, "r", encoding="cp1252", errors='ignore') as file:
+            last_line = 'empty'
+            previous_line = 'empty'
+            add_lines = ''
+            epoch_line = ''
+            time_per_epoch = ''
+            for line in file:
+                update += line == '}\n'
+                if update:
+                    for w in track_params:
+                        if w in line:
+                            add_lines += line
+                if 'Epoch' in line:
+                    epoch_line = line
+
+                if 'experiment: ' in line:
+                    add_lines += line
+
+                if 'ETA' in line:
+                    idx_ETA = line.index('ETA')
+                    time = line[idx_ETA + 4:]
+                    idx_hyphen = time.index('-')
+                    time = time[:idx_hyphen].replace(' ', '')
+                    if 's' in time:
+                        time = '0:' + time.replace('s', '')
+                    if time.count(':') == 1: time = '0:{}'.format(time)
+                    try:
+                        time = datetime.datetime.strptime(time, "%H:%M:%S") - datetime.datetime(1900, 1, 1)
+                        time = time.total_seconds()
+                    except:
+                        time = int(time.split(':')[0]) * 24 * 60 + int(time.split(':')[1]) * 60 + int(
+                            time.split(':')[2])
+                    time_per_epoch += ' {}'.format(time)
+                previous_line_2 = previous_line
+                previous_line = last_line
+                last_line = line.replace('^H', '')
+            # time per epoch
+            time_per_epoch = time_per_epoch.replace(':', '.')
+            times = [float(t) for t in time_per_epoch.split(' ') if not t == '']
+            try:
+                max_t = max(times)
+                max_t = datetime.timedelta(seconds=max_t)
+            except:
+                max_t = 'max t = ?'
+
+        f = open(summary_file, "a")
+        f.write('\n       ' + d + '\n')
+        f.write(add_lines)
+        f.write(epoch_line.replace('\n', '') + ' -> {}s/e\n'.format(max_t))
+        f.write(previous_line_2)
+        f.write(previous_line)
+        f.write(last_line)
+        f.write('\n\n')
+        f.close()
