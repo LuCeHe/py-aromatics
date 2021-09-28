0c06030ada1efc66a14e8ff8b4216139a8154b44
LuCeHe <manucelotti@gmail.com>
2021-09-28 14:24:22 -0400

    minor changes

--- KerasTools/esoteric_models/wizard_of_wikipedia.py
+++ KerasTools/esoteric_models/wizard_of_wikipedia.py
@@ -21,7 +21,7 @@ from GenericTools.KerasTools.esoteric_models.transformer import create_padding_m
 def metrics_wow(num_classes, mask_value):
     metrics = [
         # sparse_perplexity,
-        sparse_f1_on_max(num_classes),
+        # sparse_f1_on_max(num_classes),
         masked_sparse_crossentropy(mask_value),
         masked_sparse_perplexity(mask_value),
         masked_f1_on_max(num_classes, mask_value),
