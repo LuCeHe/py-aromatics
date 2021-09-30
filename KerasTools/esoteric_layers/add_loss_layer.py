7e5937f0db52baa9edefd7624c7e196432052098
LuCeHe <manucelotti@gmail.com>
2021-09-30 09:13:16 -0400

    minor changes

--- KerasTools/esoteric_layers/add_loss_layer.py
+++ KerasTools/esoteric_layers/add_loss_layer.py
@@ -2,10 +2,11 @@ import tensorflow as tf
 
 class AddLossLayer(tf.keras.layers.Layer):
 
-    def __init__(self, loss, coef=1., **kwargs):
+    def __init__(self, loss, coef=1., prefix_id='', **kwargs):
         super().__init__(**kwargs)
         self.coef = coef
         self.loss = loss
+        self.prefix_id = prefix_id
 
         try:
             self.loss_name = self.loss.name
@@ -14,6 +15,8 @@ class AddLossLayer(tf.keras.layers.Layer):
                 self.loss_name = self.loss.__name__
             except:
                 self.loss_name = str(loss)
+
+        self.loss_name = self.prefix_id + self.loss_name
 
     def call(self, inputs, training=None):
         true_output, pred_output = inputs
@@ -28,5 +31,6 @@ class AddLossLayer(tf.keras.layers.Layer):
         config = {
             'coef': self.coef,
             'loss': self.loss,
+            'prefix_id': self.prefix_id,
         }
         return dict(list(super().get_config().items()) + list(config.items()))
--- KerasTools/esoteric_layers/add_metrics_layer.py
+++ KerasTools/esoteric_layers/add_metrics_layer.py
@@ -1,23 +1,25 @@
 import tensorflow as tf
 
+
 class AddMetricsLayer(tf.keras.layers.Layer):
 
-    def __init__(self, metrics, **kwargs):
+    def __init__(self, metrics, prefix_id='', **kwargs):
         super().__init__(**kwargs)
         self.additional_metrics = metrics
+        self.prefix_id = prefix_id
 
-    def call(self, inputs, training=None):
+    def call(self, inputs, **kwargs):
         true_output, pred_output = inputs
 
         for m in self.additional_metrics:
             m_output = m(true_output, pred_output)
             name = m.name if hasattr(m, 'name') else m.__name__
-            self.add_metric(m_output, name=name, aggregation='mean')
+            self.add_metric(m_output, name=self.prefix_id + name, aggregation='mean')
         return pred_output
-
 
     def get_config(self):
         config = {
             'metrics': self.additional_metrics,
+            'prefix_id': self.prefix_id,
         }
         return dict(list(super().get_config().items()) + list(config.items()))
