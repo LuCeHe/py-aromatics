9d916c8775d24102a74cff83c120a99200f6e685
LuCeHe <manucelotti@gmail.com>
2021-12-09 13:59:15 -0500

    minor changes

--- StayOrganizedTools/submit_jobs.py
+++ StayOrganizedTools/submit_jobs.py
@@ -19,7 +19,7 @@ def run_experiments(experiments=None, init_command='python language_main.py with
         command = run_string + "'{}'".format(command)
         command = command.replace('  ', ' ')
         print('{}/{}'.format(i + 1, len(ds)), command)
-        os.system(command)
+        # os.system(command)
     print('Number jobs: {}'.format(len(ds)))
 
 
