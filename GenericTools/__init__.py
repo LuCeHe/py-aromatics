6af380cdf1f44eb79b1e58f7988b060720c1bd75
Luca Celotti <9610770+LuCeHe@users.noreply.github.com>
2022-01-14 08:09:15 +0100

    Update __init__.py

--- GenericTools/__init__.py
+++ GenericTools/__init__.py
@@ -1,1 +1,4 @@
-
+import GenericTools.KerasTools as KerasTools  
+import GenericTools.LeanguageTreatmentTools as LeanguageTreatmentTools  
+import GenericTools.PlotTools as PlotTools  
+import GenericTools.StayOrganizedTools as StayOrganizedTools  
