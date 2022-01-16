85405bc8539cb93a2b998ea82d3bae4bdb09ac53
LuCeHe <manucelotti@gmail.com>
2022-01-16 14:09:25 +0100

    minor changes

--- GenericTools/keras_tools/esoteric_losses/__init__.py
+++ GenericTools/keras_tools/esoteric_losses/__init__.py
@@ -2,7 +2,7 @@ import sys
 
 from GenericTools.keras_tools.esoteric_losses.advanced_losses import *
 from GenericTools.keras_tools.esoteric_losses.forecasting import *
-from tensorflow.keras.losses import *
+from tensorflow.keras.losses import mean_squared_error
 
 thismodule = sys.modules[__name__]
 
