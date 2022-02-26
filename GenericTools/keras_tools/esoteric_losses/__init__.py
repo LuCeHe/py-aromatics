06b7aa8a19289cf8424d254804209221273e1546
LuCeHe <manucelotti@gmail.com>
2022-02-26 17:19:11 +0100

    minor changes

--- GenericTools/keras_tools/esoteric_losses/__init__.py
+++ GenericTools/keras_tools/esoteric_losses/__init__.py
@@ -3,7 +3,8 @@ import sys
 from GenericTools.keras_tools.esoteric_losses.advanced_losses import *
 from GenericTools.keras_tools.esoteric_losses.forecasting import *
 from GenericTools.keras_tools.esoteric_losses.other_losses import *
-from tensorflow.keras.losses import mean_squared_error
+from tensorflow.keras.losses import mean_squared_error, sparse_categorical_crossentropy
+from tensorflow.keras.metrics import sparse_categorical_accuracy
 
 thismodule = sys.modules[__name__]
 
