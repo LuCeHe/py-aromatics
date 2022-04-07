2c1bb5abe0c957dcef68955382c2ce00b1a53c17
LuCeHe <manucelotti@gmail.com>
2022-04-07 21:21:27 +0200

    minor changes

--- GenericTools/stay_organized/utils.py
+++ GenericTools/stay_organized/utils.py
@@ -126,7 +126,7 @@ def collect_information():
 
 
 def setReproducible(seed=0, disableGpuMemPrealloc=True, prove_seed=True, tensorflow=False, pytorch=False,
-                    empty_cuda=True):
+                    empty_cuda=False):
     # Fix the seed of all random number generator
     random.seed(seed)
     np.random.seed(seed)
