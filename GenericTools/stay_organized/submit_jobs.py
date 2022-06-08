a24b1ab18f35e5644c5fb5114a1f5fcf115eaee5
LuCeHe <manucelotti@gmail.com>
2022-06-08 17:46:54 +0200

    minor changes

--- GenericTools/stay_organized/submit_jobs.py
+++ GenericTools/stay_organized/submit_jobs.py
@@ -6,7 +6,7 @@ def run_experiments(
         experiments=None, init_command='python language_main.py with ',
         run_string='sbatch run_tf2.sh ', is_argparse=False, sh_location='', py_location='', account='',
         duration={'days': 0, 'hours': 12, 'minutes': 0, 'prestop_training_hours': 0},
-        env_name='denv2',
+        env_name='denv2', n_gpus=0
 ):
     delta = timedelta(days=duration['days'], hours=duration['hours'], minutes=duration['minutes'])
 
@@ -18,7 +18,7 @@ def run_experiments(
     sh_duration = "{}:{}:00".format(str(int(hours)).zfill(2), str(int(minutes)).zfill(2))
 
     if run_string is None:
-        sh_name = create_sbatch_sh(sh_duration, sh_location, py_location, account, env_name)
+        sh_name = create_sbatch_sh(sh_duration, sh_location, py_location, account, env_name, n_gpus)
         run_string = 'sbatch ' + sh_name
 
     print()
@@ -52,32 +52,34 @@ def dict2iter(experiments):
     return full_ds
 
 
-def create_sbatch_sh(duration, sh_location, py_location, account, env_name):
+def create_sbatch_sh(duration, sh_location, py_location, account, env_name, n_gpus):
     sh_name = '{0:010x}'.format(int(time.time() * 256))[:15] + '.sh'
     sh_path = os.path.join(sh_location, sh_name)
     with open(sh_path, 'w') as f:
-        f.write(sh_base(duration, account, py_location, env_name))
+        f.write(sh_base(duration, account, py_location, env_name, n_gpus))
     return sh_path
 
 
-def sh_base(time, account, py_location, env_name):
+def sh_base(time, account, py_location, env_name, n_gpus):
     env_location = f'~/scratch/{env_name}/bin/activate'
     if 'cedar' in socket.gethostname():
         env_location = f'~/project/lucacehe/{env_name}/bin/activate'
     if 'gra' == socket.gethostname()[:3]:
         env_location = f'~/projects/def-jrouat/lucacehe/{env_name}/bin/activate'
+    gpus_line = ''if n_gpus==0 else f'#SBATCH --gres=gpu:{n_gpus}'
     return """#!/bin/bash
 #SBATCH --time={}
 #SBATCH --account={}
-#SBATCH --mem 64G
-#SBATCH --cpus-per-task 8
+#SBATCH --mem 32G
+#SBATCH --cpus-per-task 4
+{}
 
 module load StdEnv/2020 python/3.8 
 module load gcc/10 cuda/11.0 
 source {}
 cd {}
 $1
-""".format(time, account, env_location, py_location)
+""".format(time, account, gpus_line, env_location, py_location)
 
 #SBATCH --mem 32G
 #SBATCH --cpus-per-task 4
