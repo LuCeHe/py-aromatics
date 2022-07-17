c06c2f0cde405129fb9df164ccba04c581287032
LuCeHe <manucelotti@gmail.com>
2022-07-17 14:26:08 +0200

    minor changes

--- GenericTools/keras_tools/esoteric_tasks/wizard_of_wikipedia.py
+++ GenericTools/keras_tools/esoteric_tasks/wizard_of_wikipedia.py
@@ -1,6 +1,6 @@
 import os, shutil, json, random, h5py, argparse
 from tokenizers import Tokenizer
-from transformers import GPT2Tokenizer, AutoTokenizer, AutoConfig
+# from transformers import GPT2Tokenizer, AutoTokenizer, AutoConfig
 import tensorflow as tf
 from tensorflow.python.keras.preprocessing.sequence import pad_sequences
 from tqdm import tqdm
