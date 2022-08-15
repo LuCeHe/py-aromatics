49afc223a236b9c92e6a0d5e91c7b4000a4c41e9
LuCeHe <manucelotti@gmail.com>
2022-08-15 10:05:03 +0200

    minor changes

--- GenericTools/stay_organized/utils.py
+++ GenericTools/stay_organized/utils.py
@@ -1,4 +1,4 @@
-import argparse, logging, os, random, time, gc
+import argparse, logging, os, random, time, gc, json
 from time import strftime, localtime
 import importlib.util
 
@@ -213,6 +213,35 @@ def module_from_file(module_name, file_path):
     module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(module)
     return module
+
+
+
+class NumpyEncoder(json.JSONEncoder):
+    """ Custom encoder for numpy data types """
+
+    def default(self, obj):
+        if isinstance(obj, (np.int_, np.intc, np.intp, np.int8,
+                            np.int16, np.int32, np.int64, np.uint8,
+                            np.uint16, np.uint32, np.uint64)):
+
+            return int(obj)
+
+        elif isinstance(obj, (np.float_, np.float16, np.float32, np.float64)):
+            return float(obj)
+
+        elif isinstance(obj, (np.complex_, np.complex64, np.complex128)):
+            return {'real': obj.real, 'imag': obj.imag}
+
+        elif isinstance(obj, (np.ndarray,)):
+            return obj.tolist()
+
+        elif isinstance(obj, (np.bool_)):
+            return bool(obj)
+
+        elif isinstance(obj, (np.void)):
+            return None
+
+        return json.JSONEncoder.default(self, obj)
 
 
 if __name__ == '__main__':
