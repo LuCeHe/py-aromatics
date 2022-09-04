d077811f991aa741993cdcad06af7b546052d6bc
LuCeHe <manucelotti@gmail.com>
2022-09-04 15:22:48 +0200

    minor changes

--- GenericTools/stay_organized/utils.py
+++ GenericTools/stay_organized/utils.py
@@ -256,12 +256,13 @@ def filetail(f, lines=20):
     lines_to_go = total_lines_wanted
     block_number = -1
     blocks = []
-    # print('-' * 30)
+    print('-' * 30)
     previous_tell = np.inf
     while lines_to_go > 0 \
             and block_end_byte > 0 \
-            and f.tell() + block_number * BLOCK_SIZE > 0 \
-            and f.tell() < previous_tell:
+            and f.tell() + block_number * BLOCK_SIZE > 0 :
+            # and f.tell() < previous_tell\
+            # :
         if (block_end_byte - BLOCK_SIZE > 0):
             print(f.tell(), block_number, os.SEEK_SET)
             f.seek(f.tell() + block_number * BLOCK_SIZE, os.SEEK_SET)
