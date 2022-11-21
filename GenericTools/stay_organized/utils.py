78ac4f68d221831911d9cba4669774f60ecd65fb
LuCeHe <manucelotti@gmail.com>
2022-11-21 10:40:56 +0100

    minor changes

--- GenericTools/stay_organized/utils.py
+++ GenericTools/stay_organized/utils.py
@@ -262,7 +262,7 @@ def filetail(f, lines=20):
             and block_end_byte > 0 \
             and f.tell() + block_number * BLOCK_SIZE > 0 \
             and f.tell() <= previous_tell:
-
+        print('-' * 30)
         if (block_end_byte - BLOCK_SIZE > 0):
             print(previous_tell, f.tell(), block_number, os.SEEK_SET)
             f.seek(f.tell() + block_number * BLOCK_SIZE, os.SEEK_SET)
