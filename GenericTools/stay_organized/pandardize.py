800f78cacf1f889bc57a99067671c2ec0e88ca35
LuCeHe <manucelotti@gmail.com>
2022-12-10 18:38:49 +0100

    minor changes

--- GenericTools/stay_organized/pandardize.py
+++ GenericTools/stay_organized/pandardize.py
@@ -21,7 +21,7 @@ def simplify_col_names(df):
     return df
 
 
-def experiments_to_pandas(h5path, zips_folder, unzips_folder, extension_of_interest=['.txt', '.json', '.csv'],
+def zips_to_pandas(h5path, zips_folder, unzips_folder, extension_of_interest=['.txt', '.json', '.csv'],
                           experiments_identifier=[], exclude_files=['']):
     if isinstance(experiments_identifier, str):
         experiments_identifier = [experiments_identifier]
@@ -72,5 +72,35 @@ def experiments_to_pandas(h5path, zips_folder, unzips_folder, extension_of_inter
         df.to_hdf(h5path, key='df', mode='w')
     else:
         df = pd.read_hdf(h5path, 'df')
+    return df
+
+
+def experiments_to_pandas(h5path, zips_folder, unzips_folder, extension_of_interest=['.txt', '.json', '.csv'],
+                          experiments_identifier=[], exclude_files=[''], check_for_new=False):
+    df = zips_to_pandas(h5path, zips_folder, unzips_folder, extension_of_interest=extension_of_interest,
+                          experiments_identifier=experiments_identifier, exclude_files=exclude_files)
+
+    if check_for_new:
+        new = []
+        old = [os.path.split(p)[1] for p in df['path'].values]
+
+        for gpath in zips_folder:
+            new.extend([p.replace('.zip', '') for p in os.listdir(gpath) if 'zip' in p])
+        missing = [p for p in new if not p in old]
+
+        newh5path = h5path.replace('.h5', '_missing.h5')
+
+        if len(missing) > 0:
+            ndf = zips_to_pandas(
+                h5path=newh5path, zips_folder=zips_folder, unzips_folder=unzips_folder, experiments_identifier=missing,
+                exclude_files=['cout.txt']
+            )
+            bigdf = pd.concat([df, ndf])
+            bigdf.to_hdf(h5path, key='df', mode='w')
+            df = bigdf
+
+        if os.path.exists(newh5path):
+            os.remove(newh5path)
+
 
     return df
