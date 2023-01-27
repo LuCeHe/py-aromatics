70d5216d1d547413b6fdb5d6d1b99eb2e04f999a
LuCeHe <manucelotti@gmail.com>
2023-01-27 17:49:56 +0100

    minor changes

--- GenericTools/keras_tools/esoteric_models/model.py
+++ GenericTools/keras_tools/esoteric_models/model.py
@@ -1,16 +1,21 @@
-import copy
-import tensorflow as tf
 
-from tensorflow.python.keras.engine.training import _disallow_inside_tf_function
-from tensorflow.python.distribute.coordinator import cluster_coordinator
+import copy
+import tensorflow.compat.v2 as tf
+
+from keras import callbacks as callbacks_module
+from keras.engine import base_layer
+from keras.engine import data_adapter
+from keras.engine import training_utils
+from keras.engine.training import _get_verbosity
+from keras.optimizers.optimizer_experimental import (
+    optimizer as optimizer_experimental,
+)
+from keras.utils import tf_utils
+from keras.utils import version_utils
+
+# isort: off
 from tensorflow.python.eager import context
-from tensorflow.python.keras import callbacks as callbacks_module
-from tensorflow.python.keras.engine import base_layer
-from tensorflow.python.keras.engine import data_adapter
-from tensorflow.python.keras.engine import training_utils
-from tensorflow.python.keras.utils import tf_utils
-from tensorflow.python.keras.utils import version_utils
-from tensorflow.python.profiler import trace
+from tensorflow.python.keras.engine.training import _disallow_inside_tf_function
 
 
 class modifiedModel(tf.keras.models.Model):
@@ -239,36 +244,43 @@ class modifiedModel(tf.keras.models.Model):
             ValueError: In case of mismatch between the provided input data
                 and what the model expects or when the input data is empty.
         """
-        base_layer.keras_api_gauge.get_cell('fit').set(True)
+        base_layer.keras_api_gauge.get_cell("fit").set(True)
         # Legacy graph support is contained in `training_v1.Model`.
-        version_utils.disallow_legacy_graph('Model', 'fit')
+        version_utils.disallow_legacy_graph("Model", "fit")
         self._assert_compile_was_called()
-        self._check_call_args('fit')
-        _disallow_inside_tf_function('fit')
+        self._check_call_args("fit")
+        _disallow_inside_tf_function("fit")
 
-        if verbose == 'auto':
-            if self.distribute_strategy._should_use_with_coordinator:  # pylint: disable=protected-access
-                verbose = 2  # Default to epoch-level logging for PSStrategy.
-            else:
-                verbose = 1  # Default to batch-level logging otherwise.
+        verbose = _get_verbosity(verbose, self.distribute_strategy)
 
-        if validation_split:
-            # Create the validation data using the training data. Only supported for
-            # `Tensor` and `NumPy` input.
-            (x, y, sample_weight), validation_data = (
-                data_adapter.train_validation_split(
-                    (x, y, sample_weight), validation_split=validation_split))
+        if validation_split and validation_data is None:
+            # Create the validation data using the training data. Only supported
+            # for `Tensor` and `NumPy` input.
+            (
+                x,
+                y,
+                sample_weight,
+            ), validation_data = data_adapter.train_validation_split(
+                (x, y, sample_weight), validation_split=validation_split
+            )
 
         if validation_data:
-            val_x, val_y, val_sample_weight = (
-                data_adapter.unpack_x_y_sample_weight(validation_data))
+            (
+                val_x,
+                val_y,
+                val_sample_weight,
+            ) = data_adapter.unpack_x_y_sample_weight(validation_data)
 
-        if self.distribute_strategy._should_use_with_coordinator:  # pylint: disable=protected-access
-            self._cluster_coordinator = cluster_coordinator.ClusterCoordinator(
-                self.distribute_strategy)
+        if self.distribute_strategy._should_use_with_coordinator:
+            self._cluster_coordinator = (
+                tf.distribute.experimental.coordinator.ClusterCoordinator(
+                    self.distribute_strategy
+                )
+            )
 
-        with self.distribute_strategy.scope(), \
-             training_utils.RespectCompiledTrainableState(self):
+        with self.distribute_strategy.scope(), training_utils.RespectCompiledTrainableState(  # noqa: E501
+            self
+        ):
             # Creates a `tf.data.Dataset` and handles batch and epoch iteration.
             data_handler = data_adapter.get_data_handler(
                 x=x,
@@ -284,7 +296,8 @@ class modifiedModel(tf.keras.models.Model):
                 workers=workers,
                 use_multiprocessing=use_multiprocessing,
                 model=self,
-                steps_per_execution=self._steps_per_execution)
+                steps_per_execution=self._steps_per_execution,
+            )
 
             # Container that configures and calls `tf.keras.Callback`s.
             if not isinstance(callbacks, callbacks_module.CallbackList):
@@ -295,7 +308,8 @@ class modifiedModel(tf.keras.models.Model):
                     model=self,
                     verbose=verbose,
                     epochs=epochs,
-                    steps=data_handler.inferred_steps)
+                    steps=data_handler.inferred_steps,
+                )
 
             self.stop_training = False
             self.train_function = self.make_train_function()
@@ -305,39 +319,63 @@ class modifiedModel(tf.keras.models.Model):
             # Handle fault-tolerance for multi-worker.
             # TODO(omalleyt): Fix the ordering issues that mean this has to
             # happen after `callbacks.on_train_begin`.
-            data_handler._initial_epoch = (  # pylint: disable=protected-access
-                self._maybe_load_initial_epoch_from_ckpt(initial_epoch))
-            logs = {}
+            steps_per_epoch_inferred = (
+                steps_per_epoch or data_handler.inferred_steps
+            )
+            (
+                data_handler._initial_epoch,
+                data_handler._initial_step,
+            ) = self._maybe_load_initial_counters_from_ckpt(
+                steps_per_epoch_inferred, initial_epoch
+            )
+            logs = None
             for epoch, iterator in data_handler.enumerate_epochs():
                 self.reset_metrics()
                 callbacks.on_epoch_begin(epoch, logs)
                 with data_handler.catch_stop_iteration():
+                    data_handler._initial_step = data_handler._initial_step or (
+                        self._maybe_load_initial_step_from_ckpt()
+                    )
                     for step in data_handler.steps():
-                        with trace.Trace(
-                                'train',
+                        try:
+                            with tf.profiler.experimental.Trace(
+                                "train",
                                 epoch_num=epoch,
                                 step_num=step,
                                 batch_size=batch_size,
-                                _r=1):
-                            callbacks.on_train_batch_begin(step)
-                            tmp_logs = self.train_function(iterator)
-                            if data_handler.should_sync:
-                                context.async_wait()
-                            logs = tmp_logs  # No error, now safe to assign to logs.
-                            end_step = step + data_handler.step_increment
-                            callbacks.on_train_batch_end(end_step, logs)
-                            if self.stop_training:
-                                break
+                                _r=1,
+                            ):
+                                callbacks.on_train_batch_begin(step)
+                                tmp_logs = self.train_function(iterator)
+                                if data_handler.should_sync:
+                                    context.async_wait()
+                                # No error, now safe to assign to logs.
+                                logs = tmp_logs
+                                end_step = step + data_handler.step_increment
+                                callbacks.on_train_batch_end(end_step, logs)
+                                if self.stop_training:
+                                    break
+                        except Exception as e:
+                            print(e)
 
                 logs = tf_utils.sync_to_numpy_or_python_type(logs)
                 if logs is None:
-                    raise ValueError('Expect x to be a non-empty array or dataset.')
+                    raise ValueError(
+                        "Unexpected result of `train_function` "
+                        "(Empty logs). Please use "
+                        "`Model.compile(..., run_eagerly=True)`, or "
+                        "`tf.config.run_functions_eagerly(True)` for more "
+                        "information of where went wrong, or file a "
+                        "issue/bug to `tf.keras`."
+                    )
                 epoch_logs = copy.copy(logs)
 
                 # Run validation.
-                if validation_data and self._should_eval(epoch, validation_freq):
+                if validation_data and self._should_eval(
+                    epoch, validation_freq
+                ):
                     # Create data_handler for evaluation and cache it.
-                    if getattr(self, '_eval_data_handler', None) is None:
+                    if getattr(self, "_eval_data_handler", None) is None:
                         self._eval_data_handler = data_adapter.get_data_handler(
                             x=val_x,
                             y=val_y,
@@ -350,7 +388,8 @@ class modifiedModel(tf.keras.models.Model):
                             workers=workers,
                             use_multiprocessing=use_multiprocessing,
                             model=self,
-                            steps_per_execution=self._steps_per_execution)
+                            steps_per_execution=self._steps_per_execution,
+                        )
                     val_logs = self.evaluate(
                         x=val_x,
                         y=val_y,
@@ -362,8 +401,11 @@ class modifiedModel(tf.keras.models.Model):
                         workers=workers,
                         use_multiprocessing=use_multiprocessing,
                         return_dict=True,
-                        _use_cached_eval_dataset=True)
-                    val_logs = {'val_' + name: val for name, val in val_logs.items()}
+                        _use_cached_eval_dataset=True,
+                    )
+                    val_logs = {
+                        "val_" + name: val for name, val in val_logs.items()
+                    }
                     epoch_logs.update(val_logs)
 
                 callbacks.on_epoch_end(epoch, epoch_logs)
@@ -371,8 +413,13 @@ class modifiedModel(tf.keras.models.Model):
                 if self.stop_training:
                     break
 
-            # If eval data_hanlder exists, delete it after all epochs are done.
-            if getattr(self, '_eval_data_handler', None) is not None:
+            if isinstance(self.optimizer, optimizer_experimental.Optimizer):
+                self.optimizer.finalize_variable_values(
+                    self.trainable_variables
+                )
+
+            # If eval data_handler exists, delete it after all epochs are done.
+            if getattr(self, "_eval_data_handler", None) is not None:
                 del self._eval_data_handler
             callbacks.on_train_end(logs=training_logs)
             return self.history
--- GenericTools/keras_tools/expose_latent.py
+++ GenericTools/keras_tools/expose_latent.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 
-def expose_latent_model(original_model, exclude_layers=[], include_layers=[], idx=None, return_names = False):
+def expose_latent_model(original_model, exclude_layers=[], include_layers=[], idx=None, return_names=False):
     layer_names = []
     intermediate_layers = []
     if not isinstance(idx, list): idx = [idx]
@@ -34,20 +34,113 @@ def expose_latent_model(original_model, exclude_layers=[], include_layers=[], id
         return test_model
 
 
-
 def split_model(model, pairs):
+    # FIXME: make it work for complex networks in the intermodel
     lnames = [layer.name for layer in model.layers]
 
     input_shape = model.get_layer(lnames[pairs[0] + 1]).input_shape[1:]
-    premodel = tf.keras.models.Model(model.inputs, model.get_layer(lnames[pairs[0]]).output)
 
     DL_input = tf.keras.layers.Input(input_shape)
     DL_model = DL_input
+    layers = {}
+    inp_name = model.layers[pairs[0]].name
+    out_name = ''
     for layer in model.layers[pairs[0] + 1:pairs[1] + 1]:
+        layers[layer.name] = layer
+
         if isinstance(layer.input, list):
+            # print('letssee')
+            # print([l.name for l in layer.input])
+            # print(layers.keys())
             break
         DL_model = layer(DL_model)
+
+    premodel = tf.keras.models.Model(model.inputs, model.get_layer(lnames[pairs[0]]).output)
     intermodel = tf.keras.models.Model(inputs=DL_input, outputs=DL_model)
 
     return premodel, intermodel
 
+
+if __name__ == '__main__':
+    from alif_sg.neural_models.transformer_model import build_model
+
+    model = build_model(
+        inputs_timesteps=3,
+        target_timesteps=4,
+        inputs_vocab_size=2,
+        target_vocab_size=2,
+        encoder_count=4,
+        decoder_count=4,
+        attention_head_count=2,
+        d_model=2,
+        d_point_wise_ff=2,
+        dropout_prob=.1,
+        activation='relu',
+        comments='',
+    )
+
+    pairs = [48, 143]
+    # split_model(model, pairs)
+
+    split_layer_name = 'eidentity_3_3'
+    model2split = model
+
+    # Determine the split point based on the 'on_head' argument.
+    tail_input = tf.keras.layers.Input(batch_shape=model2split.get_layer(split_layer_name).get_input_shape_at(0))
+
+    layer_outputs = {}
+
+
+    def _find_backwards(layer):
+        """
+        Returns outputs of a layer by moving backward and
+        finding outputs of previous layers until reaching split layer.
+        directly inspired by the answer at the link below
+        with some modifications and corrections. https://stackoverflow.com/a/56228514
+        This is an internal function.
+        """
+
+        print(layer.name)
+
+        if layer.name in layer_outputs:
+            return layer_outputs[layer.name]
+
+        if layer.name == split_layer_name:
+            out = layer(tail_input)
+            layer_outputs[layer.name] = out
+            return out
+
+        # Find all the connected layers which this layer consumes their output
+        prev_layers = []
+        for node in layer.inbound_nodes:
+            try:
+                # If number of inbound layers > 1
+                prev_layers.extend(node.inbound_layers)
+            except TypeError:
+                # If number of inbound layers == 1
+                prev_layers.append(node.inbound_layers)
+
+        # Get the output of connected layers in a recursive manner
+        pl_outs = []
+        for pl in prev_layers:
+            plo = _find_backwards(pl)
+            try:
+                pl_outs.extend([plo])
+            except TypeError:
+                pl_outs.append([plo])
+
+        # Apply this layer on the collected outputs
+        print('---', pl_outs)
+        out = layer(pl_outs[0] if len(pl_outs) == 1 else pl_outs)
+        layer_outputs[layer.name] = out
+        return out
+
+
+    tail_output = _find_backwards(model2split.layers[-1])
+
+    names = ('head', 'tail')
+    # Creating head and tail models
+    head_model = tf.keras.models.Model(model2split.input, model2split.get_layer(split_layer_name).output, name=names[0])
+    tail_model = tf.keras.models.Model(tail_input, tail_output, name=names[1])
+    head_model.summary()
+    tail_model.summary()
