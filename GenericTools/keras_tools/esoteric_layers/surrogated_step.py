77d1f325fc637f99d9937e73538b477db65b5fb4
LuCeHe <manucelotti@gmail.com>
2023-02-20 22:06:02 +0100

    minor changes

--- GenericTools/keras_tools/esoteric_layers/surrogated_step.py
+++ GenericTools/keras_tools/esoteric_layers/surrogated_step.py
@@ -176,8 +176,10 @@ def SpikeFunctionAdaptiveGauss(v_scaled, dampening, sharpness, a, b, c, d, e, f,
 
     return tf.identity(z_, name="SpikeFunction"), grad
 
+
 def moved_shape(shape_name):
     sg_shape = get_asg_shape(shape_name)
+
     @tf.custom_gradient
     def SpikeFunctionAdapted(v_scaled, dampening, sharpness, a, b, c, d, e, f, g, h, i, l, m):
         z_ = tf.cast(tf.greater(v_scaled, 0.), dtype=tf.keras.backend.floatx())
@@ -451,11 +453,13 @@ def draw_pseudods():
     from GenericTools.stay_organized.mpl_tools import load_plot_settings
 
     # mpl.rcParams['font.family'] = 'serif'
+    label_fotsize = 18
+    linewidth = 3
     mpl = load_plot_settings(mpl)
 
     import matplotlib.pyplot as plt
-
-    fig, axs = plt.subplots(1, 2, gridspec_kw={'wspace': .1}, sharey=False, figsize=(10, 5))
+    # figsize=(10, 5)
+    fig, axs = plt.subplots(1, 2, gridspec_kw={'wspace': .1}, sharey=False, figsize=(8, 5))
 
     for k in possible_pseudod:
         x = tf.cast(tf.constant(np.linspace(0, 1.5, 1000)), tf.float32)
@@ -472,7 +476,7 @@ def draw_pseudods():
         cint = (int(255 * i) for i in c)
         print(cint)
         print(k, '#{:02x}{:02x}{:02x}'.format(*cint))
-        axs[0].plot(x, grad, color=c, label=clean_pseudo_name(k))
+        axs[0].plot(x, grad, color=c, label=clean_pseudo_name(k), linewidth=linewidth)
 
     n_exps = 7
     exponents = 10 ** np.linspace(-2, 1.2, n_exps) + 1
@@ -490,7 +494,7 @@ def draw_pseudods():
         cint = (int(255 * i) for i in c)
         print(cint)
         print(k, '#{:02x}{:02x}{:02x}'.format(*cint))
-        axs[1].plot(x, grad, color=c)
+        axs[1].plot(x, grad, color=c, linewidth=linewidth)
 
     n_grad = 100
     gradient = np.linspace(.4, .8, n_grad)[::-1]
@@ -498,8 +502,8 @@ def draw_pseudods():
 
     ax = fig.add_axes([.90, 0.2, .015, .6])
     ax.imshow(gradient.T, aspect='auto', cmap=cm)
-    ax.text(-0.01, 0.5, '$q$-PseudoSpike \t', va='center', ha='right', fontsize=16, transform=ax.transAxes)
-    ax.text(8, 0.5, '$q$ \t', va='center', ha='right', fontsize=16, transform=ax.transAxes)
+    # ax.text(-0.01, 0.5, '$q$-PseudoSpike \t', va='center', ha='right', fontsize=16, transform=ax.transAxes)
+    ax.text(12, 0.5, '$q$ \t', va='center', ha='right', fontsize=label_fotsize, transform=ax.transAxes)
     ax.set_yticks([])
     ax.set_xticks([])
 
@@ -507,7 +511,7 @@ def draw_pseudods():
     exponents = [round(e, 2) for e in 10 ** np.linspace(-2, 1.2, n_exps) + 1][::-1]
 
     ax.set_yticks(loc)
-    ax.set_yticklabels(exponents)
+    ax.set_yticklabels(exponents, fontsize=label_fotsize*.85)
 
     ax.yaxis.tick_right()
     ax.yaxis.set_label_position("right")
@@ -516,17 +520,20 @@ def draw_pseudods():
         ax.spines[pos].set_visible(False)
 
     # axs[0].set_xlabel('centered voltage')
-    axs[0].set_ylabel('Surrogate gradient\namplitude')
-    axs[1].set_xlabel('Centered voltage')
+    axs[0].set_ylabel('Surrogate gradient\namplitude', fontsize=label_fotsize)
+    axs[1].set_xlabel('Centered voltage', fontsize=label_fotsize)
     # axs[1].set_ylabel('surrogate gradient\namplitude')
 
     from matplotlib.lines import Line2D
     legend_elements = [Line2D([0], [0], color=pseudod_color(n), lw=4, label=clean_pseudname(n))
                        for n in [possible_pseudod[-1]] + possible_pseudod[:-1]]
-    axs[0].legend(handles=legend_elements, loc='best', bbox_to_anchor=(0.4, 0.5, 0.4, 0.5))
+    # axs[0].legend(handles=legend_elements, loc='best', bbox_to_anchor=(0.4, 0.5, 0.4, 0.5))
     for ax in axs:
         for pos in ['right', 'left', 'bottom', 'top']:
             ax.spines[pos].set_visible(False)
+
+        ax.tick_params(axis='both', which='major', labelsize=label_fotsize*.75)
+
     axs[0].set_xticks([0, 1])
     axs[1].set_xticks([0, 1])
     axs[0].set_yticks([0, 1])
@@ -553,8 +560,20 @@ def draw_legend():
     from GenericTools.stay_organized.mpl_tools import load_plot_settings
     mpl = load_plot_settings(mpl=mpl)
 
+    ncol = 3
+
+    pseudods = [possible_pseudod[-1]] + possible_pseudod[:-1] + ['ntailpseudod']
+    pseudods = [
+        'cappedskippseudod',
+        'originalpseudod',
+        'ntailpseudod',
+        'exponentialpseudod',
+        'gaussianpseudod',
+        'sigmoidalpseudod',
+        'fastsigmoidpseudod',
+    ]
     legend_elements = [Line2D([0], [0], color=pseudod_color(n), lw=4, label=clean_pseudname(n))
-                       for n in [possible_pseudod[-1]] + possible_pseudod[:-1] + ['ntailpseudod']]
+                       for n in pseudods]
 
     # Create the figure
     fig, ax = plt.subplots(figsize=(3.5, .5))
@@ -563,7 +582,7 @@ def draw_legend():
     # ax.tick_params(axis='both', which='both', bottom='off', top='off', labelbottom='off', right='off', left='off',
     #                 labelleft='off')
 
-    ax.legend(ncol=7, handles=legend_elements, loc='center')
+    ax.legend(ncol=ncol, handles=legend_elements, loc='center')
 
     ax.axis('off')
     ax.tick_params(axis='both', left='off', top='off', right='off', bottom='off', labelleft='off', labeltop='off',
@@ -571,7 +590,7 @@ def draw_legend():
     # plt.tight_layout(pad=0)
     plt.tight_layout(pad=0., w_pad=0., h_pad=0.)
 
-    plot_filename = r'legend.pdf'
+    plot_filename = rf'legend_cols{ncol}.pdf'
     # fig.tight_layout(pad=0)
     fig.savefig(plot_filename, bbox_inches='tight', pad_inches=0)
     # fig.savefig(plot_filename)
@@ -610,6 +629,6 @@ def draw_legend_mini():
 
 
 if __name__ == '__main__':
-    # draw_pseudods()
-    draw_legend()
+    draw_pseudods()
+    # draw_legend()
     # draw_legend_mini()
