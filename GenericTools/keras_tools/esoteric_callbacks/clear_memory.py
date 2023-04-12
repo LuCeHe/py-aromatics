49d3b913a7f38f5f5586c475ff45b47609d73639
LuCeHe <manucelotti@gmail.com>
2023-04-12 20:50:59 +0200

    minor changes

--- GenericTools/keras_tools/esoteric_callbacks/clear_memory.py
+++ GenericTools/keras_tools/esoteric_callbacks/clear_memory.py
@@ -6,12 +6,13 @@ from tensorflow import keras as k
 
 
 class ClearMemory(tf.keras.callbacks.Callback):
-    def __init__(self, end_of_epoch=True, end_of_batch=True, batch_frequency=1000, verbose=1):
+    def __init__(self, end_of_epoch=True, end_of_batch=True, batch_frequency=1000, verbose=1, show_gpu=True):
         super().__init__()
         self.end_of_epoch = end_of_epoch
         self.end_of_batch = end_of_batch
         self.batch_frequency = batch_frequency
         self.verbose = verbose
+        self.show_gpu = show_gpu
 
     def clear_memory(self):
         if self.verbose > 0:
@@ -22,8 +23,9 @@ class ClearMemory(tf.keras.callbacks.Callback):
             bcpup = psutil.cpu_percent(4)
             bramp = psutil.virtual_memory().percent
             bram = f"{psutil.Process().memory_info().rss / (1024 * 1024):.2f} MB"
-            print('GPU utilization')
-            GPUtil.showUtilization(all=True)
+            if self.show_gpu:
+                print('GPU utilization')
+                GPUtil.showUtilization(all=True)
 
         gc.collect()
         tf.compat.v1.keras.backend.clear_session()
@@ -37,8 +39,9 @@ class ClearMemory(tf.keras.callbacks.Callback):
             aram = f"{psutil.Process().memory_info().rss / (1024 * 1024):.2f} MB"
 
             print('\nafter')
-            print('GPU utilization')
-            GPUtil.showUtilization(all=True)
+            if self.show_gpu:
+                print('GPU utilization')
+                GPUtil.showUtilization(all=True)
 
             print(f'CPU usage %:  {bcpup}% -> {acpup}%')
             print(f'RAM memory %: {bramp}% -> {aramp}%')
