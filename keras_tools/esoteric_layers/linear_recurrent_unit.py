d84a998d60189450624acee61394736aff20de4f
LuCeHe <manucelotti@gmail.com>
2023-09-14 13:10:27 +0200

    minor changes

--- keras_tools/esoteric_layers/linear_recurrent_unit.py
+++ keras_tools/esoteric_layers/linear_recurrent_unit.py
@@ -84,10 +84,11 @@ class LinearRecurrentUnitCell(tf.keras.layers.Layer):
     def call(self, inputs, states, **kwargs):
         if not kwargs['training'] is None:
             tf.keras.backend.set_learning_phase(kwargs['training'])
-        print('rnn')
+        print('\nrnn' + '-' * 100)
 
         u = inputs
         x = states[0]
+        print(x)
 
         lambda_ = tf.exp(tf.dtypes.complex(-tf.exp(self.nu), self.theta))
 
@@ -99,11 +100,21 @@ class LinearRecurrentUnitCell(tf.keras.layers.Layer):
         gamma_ = tf.cast(self.gamma, self.dtype_)
 
         # rnn operations
-        x_ = x_ @ Lambda + gamma_ * u_ @ self.B
+        new_u = gamma_ * u_ @ self.B
+        new_x_ = tf.einsum('bi,ij->bj', x_, Lambda)
+        print('new_x_', new_x_)
+        print('new_u', new_u)
+        print('Lambda_pow', Lambda)
+        # print(new_u)
+        # print(gamma_)
+        # print(self.B)
+
+        x_ = new_x_ + new_u
+        print('x_', x_)
         y = tf.math.real(x_ @ self.C) + self.D * u
 
         output = y
-        new_state = (x,)
+        new_state = (x_,)
         return output, new_state
 
 
@@ -198,84 +209,110 @@ class LinearRecurrentUnitFFN(tf.keras.layers.Layer):
         self.built = True
 
     def call(self, inputs, training=None):
-        print('ffn')
+
+        print('\nffn' + '-' * 100)
+
         if not training is None:
             tf.keras.backend.set_learning_phase(training)
 
         u = inputs
 
-        lambda_ = tf.exp(tf.dtypes.complex(-tf.exp(self.nu), self.theta))
-        Lambda = tf.linalg.diag(lambda_)
+        lambda_ = tf.dtypes.complex(-tf.exp(self.nu), self.theta)
+        lambda_ = tf.repeat(tf.expand_dims(lambda_, axis=0), tf.shape(u)[1], axis=0)
 
         # repeat on axis 0
-        Lambda = tf.repeat(tf.expand_dims(Lambda, axis=0), tf.shape(u)[1], axis=0)
         time = tf.range(tf.shape(u)[1], dtype=tf.float32)
-        time = tf.expand_dims(tf.expand_dims(time, axis=-1), axis=-1)
-        # time = tf.repeat(tf.repeat(time, self.num_neurons, axis=1), axis=2)
-        time = tf.cast(time, self.dtype_)
+        time = tf.cast(tf.expand_dims(time, axis=-1), self.dtype_)
 
         # exponentiate by time
-        Lambda_pow = tf.pow(Lambda, time)
+        lambda_pow = lambda_ * time
+        lambda_pow = tf.exp(lambda_pow)
+        Lambda_pow = tf.linalg.diag(lambda_pow)
 
         # turning floats to complex
         u_ = tf.cast(u, self.dtype_)
         gamma_ = tf.cast(self.gamma, self.dtype_)
 
-        new_u = gamma_ * u_ @ self.B
         # rnn operations
-        # new_u.shape = (batch_size, time_steps, num_neurons), Lambda_pow.shape = (time_steps, num_neurons, num_neurons)
-        # x_.shape = (batch_size, time_steps, num_neurons)
+        new_u = gamma_ * u_ @ self.B
+        print('new_u', new_u)
+        print('Lambda_pow', Lambda_pow)
 
         x_ = tf.einsum('bti,tij->btj', new_u, Lambda_pow)
+        print('x_', x_)
+        x_ = tf.cumsum(x_, axis=1, )
+        print('x_cumsum', x_)
         y = tf.math.real(x_ @ self.C) + self.D * u
-
         output = y
         return output
 
 
-if __name__ == '__main__':
+def test_1():
     import time
+    # set tf seed
+    tf.random.set_seed(0)
 
     num_neurons = 2
-    time_steps = 10
+    time_steps = 2
     batch_size = 1
 
-    input_tensor = tf.random.normal((batch_size, num_neurons))
-    init_state = tf.random.normal((batch_size, num_neurons))
-    lru = LinearRecurrentUnitCell(num_neurons=num_neurons)
-    out = lru(input_tensor, (init_state,))
-    print(out[0].shape)
+    test_forward_pass = False
 
-    reslru = ResLRUCell(num_neurons=num_neurons)
-    out = reslru(input_tensor, (init_state,))
-    print(out[0].shape)
+    if test_forward_pass:
+        input_tensor = tf.random.normal((batch_size, num_neurons))
+        init_state = tf.random.normal((batch_size, num_neurons))
+        lru = LinearRecurrentUnitCell(num_neurons=num_neurons)
+        out = lru(input_tensor, (init_state,))
+        print(out[0].shape)
 
-    input_tensor = tf.random.normal((batch_size, time_steps, num_neurons))
-    lruffn = LinearRecurrentUnitFFN(num_neurons=num_neurons)
-    out = lruffn(input_tensor)
-    print(out.shape)
+        reslru = ResLRUCell(num_neurons=num_neurons)
+        out = reslru(input_tensor, (init_state,))
+        print(out[0].shape)
 
-    lru = LinearRecurrentUnitCell(num_neurons=num_neurons)
-    lrurnn = tf.keras.layers.RNN(lru, return_sequences=True)
-    out = lrurnn(input_tensor)
-    print(out.shape)
+        input_tensor = tf.random.normal((batch_size, time_steps, num_neurons))
+        lruffn = LinearRecurrentUnitFFN(num_neurons=num_neurons)
+        out = lruffn(input_tensor)
+        print(out.shape)
+
+        lru = LinearRecurrentUnitCell(num_neurons=num_neurons)
+        lrurnn = tf.keras.layers.RNN(lru, return_sequences=True)
+        out = lrurnn(input_tensor)
+        print(out.shape)
 
     # move parameters from lruffn to lrurnn
-    print('-' * 100)
+    print('=-.-=' * 100)
     print('hey!')
+    input_tensor = tf.random.normal((batch_size, time_steps, num_neurons)) / .1
+
+    lruffn = LinearRecurrentUnitFFN(num_neurons=num_neurons)
+    lrucell = LinearRecurrentUnitCell(num_neurons=num_neurons)
+    lrurnn = tf.keras.layers.RNN(lrucell, return_sequences=True)
+
+    _ = lruffn(input_tensor)
+    _ = lrurnn(input_tensor)
+    print('-' * 100)
+
+    names_ffn = [weight.name for weight in lruffn.weights]
+    names_rnn = [weight.name for weight in lrurnn.weights]
+    print(names_ffn)
+    print(names_rnn)
 
     lrurnn.set_weights(lruffn.get_weights())
     start_time = time.time()
     outrnn = lrurnn(input_tensor)
     rnn_time = time.time() - start_time
-    print(outrnn)
+    # print(outrnn)
 
     start_time = time.time()
     outffn = lruffn(input_tensor)
     ffn_time = time.time() - start_time
-    print(outffn)
+    # print(outffn)
 
     print('rnn time: ', rnn_time)
     print('ffn time: ', ffn_time)
 
     print(tf.reduce_sum(tf.abs(outffn - outrnn)))
+
+
+if __name__ == '__main__':
+    test_1()
