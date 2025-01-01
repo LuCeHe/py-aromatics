f62742ff7bf943c1a07bc98e74ccac949290c00f
Luca <luca.herrtti@gmail.com>
2025-01-01 21:04:10 +0100

    minor changes

--- src/pyaromatics/hf_tools/callbacks.py
+++ src/pyaromatics/hf_tools/callbacks.py
@@ -1,6 +1,69 @@
 import time
-from transformers import TrainerCallback
+import numpy as np
+
+from transformers import TrainerCallback, IntervalStrategy, logging
 from transformers.trainer_callback import ExportableState
+
+logger = logging.get_logger(__name__)
+
+
+class EarlyStoppingCallback(TrainerCallback, ExportableState):
+
+    def __init__(self, early_stopping_patience: int = 1, early_stopping_threshold: float = 0.0):
+        self.early_stopping_patience = early_stopping_patience
+        self.early_stopping_threshold = early_stopping_threshold
+        # early_stopping_patience_counter denotes the number of times validation metrics failed to improve.
+        self.early_stopping_patience_counter = 0
+        self.early_stopped = False
+
+    def check_metric_value(self, args, state, control, metric_value):
+        # best_metric is set by code for load_best_model
+        operator = np.greater if args.greater_is_better else np.less
+        if state.best_metric is None or (
+                operator(metric_value, state.best_metric)
+                and abs(metric_value - state.best_metric) > self.early_stopping_threshold
+        ):
+            self.early_stopping_patience_counter = 0
+        else:
+            self.early_stopping_patience_counter += 1
+
+    def on_train_begin(self, args, state, control, **kwargs):
+        assert args.load_best_model_at_end, "EarlyStoppingCallback requires load_best_model_at_end = True"
+        assert (
+                args.metric_for_best_model is not None
+        ), "EarlyStoppingCallback requires metric_for_best_model is defined"
+        assert (
+                args.eval_strategy != IntervalStrategy.NO
+        ), "EarlyStoppingCallback requires IntervalStrategy of steps or epoch"
+
+    def on_evaluate(self, args, state, control, metrics, **kwargs):
+        metric_to_check = args.metric_for_best_model
+        if not metric_to_check.startswith("eval_"):
+            metric_to_check = f"eval_{metric_to_check}"
+        metric_value = metrics.get(metric_to_check)
+
+        if metric_value is None:
+            logger.warning(
+                f"early stopping required metric_for_best_model, but did not find {metric_to_check} so early stopping"
+                " is disabled"
+            )
+            return
+
+        self.check_metric_value(args, state, control, metric_value)
+        if self.early_stopping_patience_counter >= self.early_stopping_patience:
+            control.should_training_stop = True
+            self.early_stopped = True
+
+    def state(self) -> dict:
+        return {
+            "args": {
+                "early_stopping_patience": self.early_stopping_patience,
+                "early_stopping_threshold": self.early_stopping_threshold,
+            },
+            "attributes": {
+                "early_stopping_patience_counter": self.early_stopping_patience_counter,
+            },
+        }
 
 
 class TimeStoppingCallback(TrainerCallback, ExportableState):
@@ -23,6 +86,7 @@ class TimeStoppingCallback(TrainerCallback, ExportableState):
     def __init__(self, max_seconds: float = 60 * 60 * 10):
         self.max_seconds = max_seconds
         self.initial_time = time.time()
+        self.time_stopped = False
 
     def time_stop(self, control):
         if time.time() - self.initial_time > self.max_seconds:
@@ -33,6 +97,7 @@ class TimeStoppingCallback(TrainerCallback, ExportableState):
             control.should_log = False
             control.should_save = False
             control.should_evaluate = False
+            self.time_stopped = True
 
     def on_evaluate(self, args, state, control, metrics, **kwargs):
         self.time_stop(control)
