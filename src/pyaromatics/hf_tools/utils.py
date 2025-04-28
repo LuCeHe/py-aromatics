dc3014ff52e949f26f7a8b9710170b9d6754c11b
Luca <luca.herrtti@gmail.com>
2025-04-28 12:07:48 +0200

    minor changes

--- src/pyaromatics/hf_tools/utils.py
+++ src/pyaromatics/hf_tools/utils.py
@@ -28,6 +28,7 @@ def get_hf_key(savekey_dir):
     # detect internet connection
     if connected_to_internet():
         print("\nConnected to the internet, logging in to Hugging Face Hub...\n")
+        print(token)
         login(token=token)
 
     # set the environment variable
