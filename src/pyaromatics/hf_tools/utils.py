487b0ebdfcc0104aa364035053dda889ad833fbb
Luca <luca.herrtti@gmail.com>
2025-09-18 15:32:31 +0200

    minor changes

--- src/pyaromatics/hf_tools/utils.py
+++ src/pyaromatics/hf_tools/utils.py
@@ -35,7 +35,7 @@ def get_hf_key(savekey_dir):
     return token
 
 
-def get_tokenizer(model_id, notes, save_dir=None, max_seq_length=None):
+def get_tokenizer(model_id, notes='', save_dir=None, max_seq_length=None):
     if save_dir is None:
         raise ValueError("save_dir must be specified")
 
