c4e14648fc2092cb37e033172c7cdd00745926f9
Luca <luca.herrtti@gmail.com>
2025-09-19 18:39:26 +0200

    minor changes

--- src/pyaromatics/stay_organized/utils.py
+++ src/pyaromatics/stay_organized/utils.py
@@ -1,4 +1,4 @@
-import argparse, logging, os, random, time, gc, json, re, shutil, functools
+import argparse, logging, os, random, time, gc, json, re, shutil, functools, socket
 
 from tqdm import tqdm
 from itertools import groupby
@@ -499,6 +499,20 @@ def error_handler_with_sleep(sleep_seconds=5):
 
     return decorator
 
+
+def check_internet(host="8.8.8.8", port=53, timeout=3):
+    """
+    Host: 8.8.8.8 (Google DNS)
+    OpenPort: 53/tcp
+    Service: domain (DNS/TCP)
+    """
+    try:
+        socket.setdefaulttimeout(timeout)
+        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+        return True
+    except socket.error as ex:
+        print(f"No internet connection: {ex}")
+        return False
 
 if __name__ == '__main__':
     test_is_progress_bar()
